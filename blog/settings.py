56f87629e8f87864319af2a64a560bcb178b141f
Anushka <anushka@devtron.ai>
2020-06-02 13:50:03 +0530

    change in host address

--- blog/settings.py
+++ blog/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ DATABASES = {
         'NAME': 'sql_data',
         'USER': 'root',
         'PASSWORD': 'password',
-        'HOST': '/django-sql-dev-mysql',
+        'HOST': '/opt/lampp/var/mysql/mysql.sock',
         'PORT': '3306',
     }
 }
